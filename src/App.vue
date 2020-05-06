4160543280a7ed6fc829cbf88c978042e04c538d
wuyl <wuyl@yxt.com>
2020-05-06 18:04:01 +0800

    test:render函数

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
   <section>
-    <h2>{{ title }}</h2>
+    <h2>{{ state.count }}</h2>
+    <p>{{state.double}}</p>
+    <button @click="increment">add</button>
+    <span class="side">{{a}}</span>
+    {{count2}}
   </section>
 </template>
 
 <script>
+import {h, reactive, computed, watchEffect, ref, onMounted } from 'vue'
 export default {
   setup() {
-    return {
-      title: "Hello World Vue3.0",
-    };
+    const state = reactive({
+      count: 0,
+      double: computed(() => {
+        return state.count * 2
+      })
+    })
+    
+    const count2 = ref(0)
+    const a = computed(() => {
+        return state.count * 2
+      })
+    function increment () {
+      state.count++
+      count2.value++
+    }
+    onMounted(() => {
+      console.log('component is mounted!')
+    })
+    watchEffect(() => {
+      // console.log(state.count)
+      console.log(22)
+    })
+    return () => h('div', [state.double, state.count])
   },
 };
 </script>
@@ -19,6 +44,5 @@ body {
   padding: 50px;
   color: #42b983;
   text-align: center;
-  background-color: #282c34;
 }
 </style>
